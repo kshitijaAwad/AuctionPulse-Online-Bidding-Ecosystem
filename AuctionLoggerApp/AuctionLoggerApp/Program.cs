4a7e4a41b8d9f0a56b59c315d8dcbca6c9abb173
Vedanti Dhole <vedanti.dhole03@gmail.com>
2026-02-06 14:40:25 +0530

    Initial commit - AuctionPulseProject

---
+++ AuctionLoggerApp/AuctionLoggerApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LoggerLib;
+
+namespace AuctionLoggerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = args.Length > 0 ? args[0] : "Default log message";
+
+            FileLogger.CurrentLogger.Log(message);
+
+            Console.WriteLine("Logged: " + message);
+        }
+    }
+}
---
+++ LoggerLib/LoggerLib/FileLogger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace LoggerLib
+{
+    public class FileLogger
+    {
+        private static FileLogger logger = new FileLogger();
+
+        private FileLogger()
+        {
+        }
+
+        public static FileLogger CurrentLogger
+        {
+            get { return logger; }
+        }
+
+        public void Log(string message)
+        {
+            string directory = "D:\\CDAC\\ProjectLogger";
+
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            string filePath = Path.Combine(directory, "log.txt");
+
+            FileStream stream;
+
+            if (File.Exists(filePath))
+                stream = new FileStream(filePath, FileMode.Append, FileAccess.Write);
+            else
+                stream = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+
+            string messageData = "Logged at " + DateTime.Now + " - " + message;
+
+            StreamWriter writer = new StreamWriter(stream);
+            writer.WriteLine(messageData);
+
+            writer.Close();
+            stream.Close();
+        }
+    }
+}
